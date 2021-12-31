61c7289540bdf887d8c16436f13ef86f0d9a91d0
wangxiaomie <1512342730@qq.com>
2021-12-31 21:55:43 +0800

    改了logintabfragment，测试了虚空接口的跳转功能

--- MyApplication1/app/src/main/java/com/example/myapplication1/LoginTabFragment.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/LoginTabFragment.java
@@ -43,113 +43,96 @@ public class LoginTabFragment extends Fragment {
         username=root.findViewById(R.id.username);
         password=root.findViewById(R.id.password);
         btn_login=root.findViewById(R.id.btn_login);
-//        login=root.findViewById(R.id.login);
-//        adm_entry=root.findViewById(R.id.adm_entry);
-//        forgotpassword=root.findViewById(R.id.forgotpassword);
-//
-//        //切换为"注册"时的动画设置
-//        //位置变化
-//        username.setTranslationY(800);
-//        password.setTranslationY(800);
-//        //透明度
-//        username.setAlpha(v);
-//        password.setAlpha(v);
-//        //动画
-//        username.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-//        password.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-
 //        //登录按钮的响应
-//        login.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //捕获按键对应的文本
-//                String _name=username.getText().toString();
-//                String _password=password.getText().toString();
-//
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        try {
-//                            FormBody.Builder params = new FormBody.Builder();
-//                            params.add("name", _name);
-//                            params.add("passwd", _password);
-//
-//
-//                            OkHttpClient client = new OkHttpClient();
-//                            Request request = new Request.Builder()
-//                                    .url("http://47.103.9.250:9000/api/v1/userservice/login")
-////                                    .url("http://127.0.0.1:9000/api/v1/userservice/registration")
-//                                    .post(params.build())
-//                                    .build();
-//                            Response response = client.newCall(request).execute();
-//                            String responseData = response.body().string();
-//                            JSONObject jsonObject = new JSONObject(responseData);
-//                            Log.d("msg", jsonObject.getString("msg"));
-//                            Log.d("object", jsonObject.getString("object"));
-//                            String object = jsonObject.getString("object");
-//                            String code=jsonObject.getString("code");
-//
-//                            if(code.equals("200")){
-//
-//                                SharedPreferences spf = getActivity().getSharedPreferences("spf",Context.MODE_PRIVATE);
-//                                SharedPreferences.Editor editor=spf.edit();
-//                                editor.putString("name",_name);
-//                                editor.putString("password",_password);
-//                                editor.apply();
-////                                Intent intenttest=new Intent(getActivity(),ProfileFragment.class);
-//
-////                                startActivity(intenttest);
-//                                getActivity().runOnUiThread( new  Runnable() {
-//                                    @Override
-//                                    public  void  run() {
-//                                        Toast toastCenter = Toast.makeText(getActivity(), "登录成功", Toast.LENGTH_LONG);
-//                                        //确定Toast显示位置，并显示
-//                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
-//                                        toastCenter.show();
-//                                    }
-//                                });
-//                                if(object.equals("1")){
-//                                    Intent intent = new Intent(getActivity(), MainActivity.class);
-//                                    intent.putExtra("name",_name);
-//                                    startActivity(intent);
-//                                }
-//                                else {
-//                                    Intent intent = new Intent(getActivity(), DrivermainActivity.class);
-//                                    intent.putExtra("name",_name);
-//                                    startActivity(intent);
-//                                }
-//
-//                            }
-//                            else {
-//                                getActivity().runOnUiThread( new  Runnable() {
-//                                    @Override
-//                                    public  void  run() {
-//                                        Toast toastCenter = Toast.makeText(getActivity(), "登录失败", Toast.LENGTH_LONG);
-//                                        //确定Toast显示位置，并显示
-//                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
-//                                        toastCenter.show();
-//                                    }
-//                                });
-//                            }
-//
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                            getActivity().runOnUiThread( new  Runnable() {
-//                                @Override
-//                                public  void  run() {
-//                                    Toast toastCenter = Toast.makeText(getActivity(), "登录失败", Toast.LENGTH_LONG);
-//                                    //确定Toast显示位置，并显示
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //捕获按键对应的文本
+                String _name=username.getText().toString();
+                String _password=password.getText().toString();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            FormBody.Builder params = new FormBody.Builder();
+                            params.add("name", _name);
+                            params.add("passwd", _password);
+
+
+                            OkHttpClient client = new OkHttpClient();
+                            Request request = new Request.Builder()
+                                    .url("http://47.103.9.250:9000/api/v1/userservice/login")
+                                    .post(params.build())
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            String responseData = response.body().string();
+                            JSONObject jsonObject = new JSONObject(responseData);
+                            Log.d("msg", jsonObject.getString("msg"));
+                            Log.d("object", jsonObject.getString("object"));
+                            String object = jsonObject.getString("object");
+                            String code=jsonObject.getString("code");
+
+                            if(code.equals("200")){
+
+                                SharedPreferences spf = getActivity().getSharedPreferences("spf",Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor=spf.edit();
+                                editor.putString("name",_name);
+                                editor.putString("password",_password);
+                                editor.apply();
+                                getActivity().runOnUiThread( new  Runnable() {
+                                    @Override
+                                    public  void  run() {
+                                        Toast toastCenter = Toast.makeText(getActivity(), "登录成功", Toast.LENGTH_LONG);
+                                        //确定Toast显示位置，并显示
+                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                        toastCenter.show();
+                                    }
+                                });
+                                if(object.equals("1")){//
+                                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                                    intent.putExtra("name",_name);
+                                    startActivity(intent);
+                                } //教师和学生的判断
+                                else {
+                                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                                    intent.putExtra("name",_name);
+                                    startActivity(intent);
+                                }
+
+                            }
+                            else {
+                                getActivity().runOnUiThread( new  Runnable() {
+                                    @Override
+                                    public  void  run() {
+                                        Toast toastCenter = Toast.makeText(getActivity(), "请求成功，登陆失败", Toast.LENGTH_LONG);
+                                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                        toastCenter.show();
+
+                                    }
+                                });
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            getActivity().runOnUiThread( new  Runnable() {
+                                @Override
+                                public  void  run() {
+//                                    Toast toastCenter = Toast.makeText(getActivity(), "请求失败，登陆失败", Toast.LENGTH_LONG);
 //                                    toastCenter.setGravity(Gravity.CENTER, 0, 0);
 //                                    toastCenter.show();
-//                                }
-//                            });
-//                        }
-//
-//                    }
-//                }).start();
-//
-//            }
-//        });
+                                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                                    intent.putExtra("name",_name);
+                                    startActivity(intent);
+                                }
+                            });
+                        }
+
+                    }
+                }).start();
+
+            }
+        });
         return root;
 
     }
