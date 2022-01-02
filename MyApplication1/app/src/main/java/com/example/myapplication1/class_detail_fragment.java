36305d61d2fe5602afc3e16a7f577e83b624423f
GreymaneCode <421789165@qq.com>
2022-01-02 17:32:54 +0800

    class_detail_ok

--- MyApplication1/app/src/main/java/com/example/myapplication1/class_detail_fragment.java
+++ MyApplication1/app/src/main/java/com/example/myapplication1/class_detail_fragment.java
@@ -1,37 +1,56 @@
 package com.example.myapplication1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class class_detail_fragment extends Fragment{
-    Button class_detail_btn_back,class_detail_btn_stu_list,class_detail_homework,class_detail_sign,class_detail_source;
-    TextView class_detail_txt_name,class_detail_txt_course,class_detail_txt_slot,class_detail_txt_code;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class class_detail_fragment extends Fragment {
+    String class_code;
+    Button class_detail_btn_back, class_detail_btn_stu_list, class_detail_homework, class_detail_sign, class_detail_source;
+    TextView class_detail_txt_name, class_detail_txt_course, class_detail_txt_slot, class_detail_txt_code;
     ImageView class_detail_icon;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_class_detail, container, false);
-        class_detail_btn_back=root.findViewById(R.id.class_detail_btn_back);
-        class_detail_btn_stu_list=root.findViewById(R.id.class_detail_btn_stu_list);
-        class_detail_homework=root.findViewById(R.id.class_detail_homework);
-        class_detail_sign=root.findViewById(R.id.class_detail_sign);
-        class_detail_source=root.findViewById(R.id.class_detail_source);
+        class_detail_btn_back = root.findViewById(R.id.class_detail_btn_back);
+        class_detail_btn_stu_list = root.findViewById(R.id.class_detail_btn_stu_list);
+        class_detail_homework = root.findViewById(R.id.class_detail_homework);
+        class_detail_sign = root.findViewById(R.id.class_detail_sign);
+        class_detail_source = root.findViewById(R.id.class_detail_source);
 
-        class_detail_icon=root.findViewById(R.id.class_detail_icon);
+        class_detail_icon = root.findViewById(R.id.class_detail_icon);
 
-        class_detail_txt_name=root.findViewById(R.id.class_detail_txt_name);
-        class_detail_txt_course=root.findViewById(R.id.class_detail_txt_course);
-        class_detail_txt_slot=root.findViewById(R.id.class_detail_txt_slot);
-        class_detail_txt_code=root.findViewById(R.id.class_detail_txt_code);
+        class_detail_txt_name = root.findViewById(R.id.class_detail_txt_name);
+        class_detail_txt_course = root.findViewById(R.id.class_detail_txt_course);
+        class_detail_txt_slot = root.findViewById(R.id.class_detail_txt_slot);
+        class_detail_txt_code = root.findViewById(R.id.class_detail_txt_code);
+        getClassInfo();//为上面4个textview赋值
 
         class_detail_btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,15 +59,14 @@ public class class_detail_fragment extends Fragment{
                     @Override
                     public void run() {
                         try {
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new course_stu_fragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new course_stu_fragment()).commit();
 
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             e.printStackTrace();
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new course_stu_fragment()).commit();
+                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new course_stu_fragment()).commit();
                                 }
                             });
                         }
@@ -65,15 +83,14 @@ public class class_detail_fragment extends Fragment{
                     @Override
                     public void run() {
                         try {
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new stu_list_fragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new stu_list_fragment()).commit();
 
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             e.printStackTrace();
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new stu_list_fragment()).commit();
+                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new stu_list_fragment()).commit();
                                 }
                             });
                         }
@@ -90,15 +107,14 @@ public class class_detail_fragment extends Fragment{
                     @Override
                     public void run() {
                         try {
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new stu_list_fragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new stu_list_fragment()).commit();
 
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             e.printStackTrace();
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new stu_list_fragment()).commit();
+                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new stu_list_fragment()).commit();
                                 }
                             });
                         }
@@ -107,7 +123,7 @@ public class class_detail_fragment extends Fragment{
                 }).start();
             }
         });
-        //signlist还没写,sign写了
+
         class_detail_sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -115,15 +131,14 @@ public class class_detail_fragment extends Fragment{
                     @Override
                     public void run() {
                         try {
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new sign_list_fragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new sign_list_fragment()).commit();
 
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             e.printStackTrace();
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new sign_list_fragment()).commit();
+                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new sign_list_fragment()).commit();
                                 }
                             });
                         }
@@ -140,15 +155,14 @@ public class class_detail_fragment extends Fragment{
                     @Override
                     public void run() {
                         try {
-                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new stu_list_fragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new stu_list_fragment()).commit();
 
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             e.printStackTrace();
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new stu_list_fragment()).commit();
+                                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new stu_list_fragment()).commit();
                                 }
                             });
                         }
@@ -157,13 +171,80 @@ public class class_detail_fragment extends Fragment{
                 }).start();
             }
         });
-//        取数据后赋值
-//        class_detail_txt_name.setText("a");
-//        class_detail_txt_course.setText("b");
-//        class_detail_txt_slot.setText("c");
-//        class_detail_txt_code.setText("d");
 
         return root;
     }
 
+    public void getClassInfo() {
+        class_code="2";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {                      //android studio版本问题    已解决
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+                            .url("http://121.37.172.109:9000/back_end/class/getClassDetail?class_code=" + class_code)
+                            .get()
+                            .build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    JSONObject jsonObject = new JSONObject(responseData);
+                    Log.d("msg", jsonObject.getString("msg"));
+                    Log.d("code", jsonObject.getString("code"));
+                    String code = jsonObject.getString("code");
+//                            String code=jsonObject.getString("code");
+                    JSONObject data = new JSONObject(jsonObject.getString("data"));
+
+                    if (code.equals("0")) {
+
+                        SharedPreferences spf = getActivity().getSharedPreferences("spf", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = spf.edit();
+                        editor.putString("class_code", "2");
+                        editor.apply();
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Log.d("data", "abc" + data.getString("classCode"));
+                                    //        取数据后赋值
+
+                                    class_detail_txt_code.setText(data.getString("classCode"));
+                                    class_detail_txt_name.setText(data.getString("classTeacherId"));
+
+                                    class_detail_txt_course .setText(data.getString("className"));
+                                    class_detail_txt_slot.setText(data.getString("classSlot"));
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    } else {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast toastCenter = Toast.makeText(getActivity(), "请求成功，登陆失败", Toast.LENGTH_LONG);
+                                toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                                toastCenter.show();
+
+                            }
+                        });
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast toastCenter = Toast.makeText(getActivity(), "请求失败，登陆失败", Toast.LENGTH_LONG);
+                            toastCenter.setGravity(Gravity.CENTER, 0, 0);
+                            toastCenter.show();
+                        }
+                    });
+                }
+
+            }
+        }).start();
+
+    }
 }
